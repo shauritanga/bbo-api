7bda22844f7a3dff2acb6e43caeb11e0001602d5
shauritanga <curtisisaac36@gmail.com>
2024-07-25 17:38:33 +0300

    refactor employee controller

--- controllers/employee.js
+++ controllers/employee.js
@@ -2,28 +2,33 @@ const nodemailer = require("nodemailer");
 const Employee = require("../models/employee.js");
 
 let transporter = nodemailer.createTransport({
-  service: "Gmail",
+  host: "mail.alphafunds.co.tz",
   auth: {
-    user: "curtisisaac36@gmail.com", // replace with your email
-    pass: "mpee ioxs pbyv juss", // replace with your password
+    user: "admin@alphafunds.co.tz", // replace with your email
+    pass: "vct6?99^i}^]", // replace with your password
   },
 });
 module.exports.addEmployee = async (req, res) => {
   const { email } = req.body;
-  const employee = new Employee({ ...req.body });
-  const employeeResults = await employee.save();
-  if (!employeeResults) {
-    return res.status(400).json({ message: "Failed to add employee" });
+  try {
+    const employee = new Employee({ ...req.body });
+    const employeeResults = await employee.save();
+
+    const passwordResetLink = `https://admin.alphafunds.co.tz/reset-password?email=${email}`;
+
+    const mailOptions = {
+      from: '"Alpha Capital" admin@alphafunds.co.tz',
+      to: email,
+      subject: "Account Activation",
+      text: `Click the following link to change your password: ${passwordResetLink}`,
+    };
+    await transporter.sendMail(mailOptions);
+    res.status(201).json(employeeResults);
+  } catch (error) {
+    console.log(error);
+    if (error.code === 11000) {
+      return res.status(500).json({ message: "User already exists" });
+    }
+    return res.status(500).json({ message: error.message });
   }
-
-  const passwordResetLink = `https://admin.alphafunds.co.tz/reset-password?email=${email}`;
-
-  const mailOptions = {
-    from: "curtisisaac36@gmail.com",
-    to: email,
-    subject: "Account Activation",
-    text: `Click the following link to change your password: ${passwordResetLink}`,
-  };
-  await transporter.sendMail(mailOptions);
-  res.status(201).json(employeeResults);
 };
